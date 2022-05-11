dc93d313d02cce0adbc7cc08974aca8a5ad975d3
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2022-05-10 23:28:43 -0700

    Create Question3.py

---
+++ Question3.py
@@ -0,0 +1,8 @@
+a = input("Enter first number : ")
+b = input("Enter second number: ")
+print("Before swap: a = " + a + ", b = "+b)
+temp = a
+a = b 
+b = temp
+print("After swap: a = " + a + ", b = "+b)
+
